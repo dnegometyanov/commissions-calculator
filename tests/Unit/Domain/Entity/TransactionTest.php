68cf42ad44ba98c690c8dd193f2aea073d9d542b
dnegometyanov <denis.engometyanov@gmail.com>
2021-06-06 13:21:47 +0300

    Fixed TransactionTest

--- tests/Unit/Domain/Entity/TransactionTest.php
+++ tests/Unit/Domain/Entity/TransactionTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CommissionsTest\Unit\Domain\Entity;
 
+use Brick\Money\Currency;
 use Brick\Money\Money;
 use Commissions\CalculatorContext\Domain\Entity\Transaction;
 use Commissions\CalculatorContext\Domain\Entity\User;
@@ -21,17 +22,21 @@ class TransactionTest extends TestCase
     {
         $userMock = $this->createMock(User::class);
         $userMock->method("getId")->willReturn(1);
-        $userMock->method("getUserType")->willReturn(UserType::private());
+        $userMock->method("getUserType")->willReturn(UserType::of('private'));
 
         $transaction = new Transaction(
             Uuid::uuid4(),
             new \DateTimeImmutable('2021-01-01 12:00:00'),
             $userMock,
-            TransactionType::deposit(),
+            TransactionType::of('deposit'),
             Money::of('100.00', 'EUR')
         );
 
         $this->assertInstanceOf(Transaction::class, $transaction);
+        $this->assertTrue($transaction->getCurrency()->is(Currency::of('EUR')));
+        $this->assertEquals('EUR', $transaction->getCurrencyCode());
+        $this->assertTrue($transaction->getUserType()->is(UserType::of('private')));
+        $this->assertEquals('EUR 100.00', (string)$transaction->getAmount());
         $this->assertNotNull($transaction->getUuid());
         $this->assertEquals('2021-01-01 12:00:00', $transaction->getDateTime()->format('Y-m-d H:i:s'));
     }
